f77131c8576bedcf0053e405f79774745d2c71b6
Guisfortis <86068082+Guisfortis@users.noreply.github.com>
2021-12-02 20:05:02 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-with open('D:\list.txt') as file:
-    count = 0
-    depths = [line.strip() for line in file]
-    for i in range(3, len(depths)):
-        if sum([int(depths[i]), int(depths[i - 1]), int(depths[i - 2])]) > sum([int(depths[i - 1]), int(depths[i - 2]), int(depths[i - 3])]):
-            count += 1
-print(count)
+def movements_list():
+    with open('D:\list.txt') as file:
+        movements = [line.strip() for line in file]
+    return movements
+
+
+def movements_dict():
+    movements = {'forward': 0, 'depths': 0, 'aim': 0}
+    for i in movements_list():
+        if i[:-2] == 'forward':
+            movements['forward'] += int(i[-1])
+            movements['depths'] += (movements['aim'] * int(i[-1]))
+        elif i[:-2] == 'up':
+            movements['aim'] -= int(i[-1])
+        elif i[:-2] == 'down':
+            movements['aim'] += int(i[-1])
+    return movements
+
+
+print(movements_dict()['forward'] * movements_dict()['depths'])
