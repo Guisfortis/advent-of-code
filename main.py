ce3f8267814af9c60aeb89ba0981f34a58a4df18
Guisfortis <86068082+Guisfortis@users.noreply.github.com>
2021-12-02 20:19:36 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+with open('D:\list.txt') as file:
+    depths = [int(line.strip()) for line in file]
+count = 0
+for i in range(3, len(depths)):
+    if sum([depths[i - 2 : i + 1]) > sum([depths[i - 3 : i]):
+        count += 1
+print(count)
