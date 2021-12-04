65587b1af0a5ab186dc5b91d4085caf03eb6082f
Guisfortis <86068082+Guisfortis@users.noreply.github.com>
2021-12-04 18:23:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 with open('D:\list.txt') as file:
     depths = [int(line.strip()) for line in file]
 count = 0
+print(depths)
 for i in range(3, len(depths)):
-    if sum([depths[i - 2 : i + 1]) > sum([depths[i - 3 : i]):
+    if sum(depths[i - 2: i + 1]) > sum(depths[i - 3: i]):
         count += 1
 print(count)
